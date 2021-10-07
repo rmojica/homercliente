db0729efc0fc93edbc9ca28af14e00a03810ae5f
Rolando Sanchez <rolando@laescala.cl>
2021-10-07 00:07:37 -0600

    ultimos cambios

--- src/pipes/safe-html/safe-html.ts
+++ src/pipes/safe-html/safe-html.ts
@@ -12,8 +12,12 @@ export class SafeHtmlPipe implements PipeTransform {
   /**
    * Takes a value and makes it lowercase.
    */
-  transform(Unixdate:number) {  
-   return  new Date(Unixdate * 1000).toISOString().slice(0, 19).replace('T', ' ')
+  transform(Unixdate:number) {
+   let f =  new Date(Unixdate * 1000).toISOString().slice(0, 19);
+
+   console.log(f);
+   return f;
+
   }
-  
+
 }
